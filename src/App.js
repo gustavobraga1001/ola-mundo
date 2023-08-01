43efc4a646637c1b2e7612e9795fb3c1ce1051e1
gustavobraga1001 <gubraro0@gmail.com>
2023-08-01 16:45:23 -0300

    criando paginas com js puro

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import SobreMim from "./pages/SobreMim";
+import Home from "./pages/Home";
+
+console.log(window.location);
+
+const pagina = window.location.pathname === "/" ? <Home /> : <SobreMim />
 
 function App() {
   return (
     <div className="App">
-      <h1>Olá Mundo</h1>
+      {pagina}
     </div>
   );
 }
---
+++ src/pages/SobreMim/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SobreMim = () => {
+  return (
+    <div>Sobre Mim...</div>
+  )
+}
+
+export default SobreMim
